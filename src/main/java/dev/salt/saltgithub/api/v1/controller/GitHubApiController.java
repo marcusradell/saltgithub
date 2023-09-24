7d22d649e9f8e238ec9283848390343e72fe68eb
Carl-Fredrik Skönebrant <carl-fredrik.skonebrant@seb.se>
2023-09-24 21:17:40 +0200

    Added test for setup endpoint

--- src/main/java/dev/salt/saltgithub/api/v1/controller/GitHubApiController.java
+++ src/main/java/dev/salt/saltgithub/api/v1/controller/GitHubApiController.java
@@ -1,16 +1,15 @@
 package dev.salt.saltgithub.api.v1.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/saltgithub")
+@AllArgsConstructor
 public class GitHubApiController {
 
-    @GetMapping("status")
-    public void status() {
-
-    }
-
+  @GetMapping("status")
+  public void status() {}
 }
--- src/test/java/dev/salt/saltgithub/api/v1/controller/GitHubApiControllerTest.java
+++ src/test/java/dev/salt/saltgithub/api/v1/controller/GitHubApiControllerTest.java
@@ -1,15 +1,26 @@
 package dev.salt.saltgithub.api.v1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GitHubApiControllerTest {
 
-    @Autowired
-    private final GitHubApiController gitHubApiController;
+  @Autowired private GitHubApiController gitHubApiController;
+  @Autowired private MockMvc mockMvc;
 
-
+  @SneakyThrows
+  @Test
+  void shouldReturn200Ok() {
+    mockMvc.perform(get("/api/v1/saltgithub/status")).andExpect(status().isOk());
+  }
 }
